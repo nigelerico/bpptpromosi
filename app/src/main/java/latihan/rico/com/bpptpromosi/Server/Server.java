619c0aa94c1e1cceec93ece3123d23c3efe80b45
Bizon <adventchristopher21@gmail.com>
2019-11-16 16:38:19 +0700

    update server

--- app/src/main/java/latihan/rico/com/bpptpromosi/Server/Server.java
+++ app/src/main/java/latihan/rico/com/bpptpromosi/Server/Server.java
@@ -1,4 +1,7 @@
 package latihan.rico.com.bpptpromosi.Server;
 
 public class Server {
+    public static final String URL_API = "http://dpmptsp.tulungagung.go.id/api/android/";
+    public static final String URL_GAMBAR = "http://dpmptsp.tulungagung.go.id/api/gambar/";
+    public static final String URL_DOKUMEN = "http://dpmptsp.tulungagung.go.id/api/dokumen/";
 }
