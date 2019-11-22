2fe4c7e2faa52c25beb13603ba8be7386b33bcd7
Bizon <adventchristopher21@gmail.com>
2019-11-22 17:52:33 +0700

    Splash Screen

---
+++ app/src/main/java/latihan/rico/com/bpptpromosi/Activity/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package latihan.rico.com.bpptpromosi.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import latihan.rico.com.bpptpromosi.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+
+        Thread timer = new Thread(){
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+        timer.start();
+
+    }
+}
